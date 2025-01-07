36cad7dcec72b262a6bb41b0612deb737d9d6f99
Ильнур <ilnurik05@mail.ru>
2025-01-08 01:27:27 +0300

    Написал основу для дальнейшей работы с базой данных для каждого отдельного участника.

--- Server/server.py
+++ Server/server.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import psycopg2
+from decouple import config
 
-app = Flask(__name__)
 
-
-@app.route('/data', mathods=['GET'])
-def get_data():
-    return {"YES": "NO"}
+class Server:
+    def __init__(self):
+        try:
+            host = config("Host", cast=str)
+            port = config("Port", cast=str)
+            database = config("Database_Name", cast=str)
+            username = config("Username", cast=str)
+            password = config("Password", cast=str)
+            self.conn = psycopg2.connect(database=database, user=username, password=password, host=host, port=port)
+        except Exception as Error:
+            print("Can't establish connection to database")
+            print(Error)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    server = Server()
